8be7ee87c8e97c82d0b244e09a8b2102d8edc776
rahul1809 <rahul.mishra2003@gmail.com>
2019-09-13 22:08:34 +0530

    adding setup.sh script for building, cleaning and installing package

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
 from setuptools import setup
+from setuptools import find_packages
 
 setup(name='pyExplore',
-                 version='0.0.0',
-                 description='Python Package for exploratory data analysis in Data Science',
-                 long_description=open('README.md').read().strip(),
-                 long_description_content_type='text/markdown',
-                 author='Rahul Mishra',
-                 author_email='rahul.mishra2003@gmail.com',
-                 url='https://github.com/rahul1809/pyExplore',
-                 py_modules=['pyExplore'],
-                 install_requires=['pandas', 'matplotlib', 'numpy', 'scipy'],
-                 license='MIT License',
-                 zip_safe=False,
-                 keywords='Exploratory Data Analysis, Data Analysis,EDA',
-                 classifiers=["License :: OSI Approved :: MIT License",
-                              "Programming Language :: Python :: 3",
-                              "Programming Language :: Python :: 3.7"
-                              ])
+      version='0.0.0.1',
+      description='Python Package for exploratory data analysis in Data Science',
+      long_description=open('README.md').read().strip(),
+      long_description_content_type='text/markdown',
+      author='Rahul Mishra',
+      packages=find_packages(),
+      author_email='rahul.mishra2003@gmail.com',
+      url='https://github.com/rahul1809/pyExplore',
+      py_modules=['src'],
+      install_requires=['pandas', 'matplotlib', 'numpy', 'scipy'],
+      license='MIT License',
+      zip_safe=False,
+      keywords='Exploratory Data Analysis, Data Analysis,EDA',
+      classifiers=["License :: OSI Approved :: MIT License",
+                   "Programming Language :: Python :: 3",
+                   "Programming Language :: Python :: 3.7",
+                   "Intended Audience :: Developers",
+                   "Intended Audience :: Science/Research",
+                   "Intended Audience :: Information Technology",
+                   'Topic :: Scientific/Engineering :: Information Analysis',
+                   'Topic :: Software Development',
+                   'Topic :: Utilities'
+                   ])
---
+++ setup.sh
@@ -0,0 +1,45 @@
+#build python package build dependency
+operation=$1
+
+function setup() {
+
+  pip install wheel twine matplotlib numpy pandas scikit-learn
+}
+
+function install() {
+    pip install dist/*.tar.gz
+}
+
+function build() {
+
+      #Uploading the created python package for testing
+      python setup.py sdist bdist_wheel
+
+}
+
+
+function release() {
+    python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+}
+
+function clean() {
+ #delete old packages create
+ rm -rf dist/* build/*
+ pip  uninstall pyExplore -y
+}
+
+if [ $operation == "clean" ]
+then
+  clean
+elif [ $operation == "build" ]
+then
+  build
+elif [ $operation == "setup" ]
+then
+  setup
+elif [ $operation == "release" ]
+then
+  release
+elif [ $operation == "install" ]; then
+  install
+fi
